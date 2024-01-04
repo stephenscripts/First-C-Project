cdc4a9ab61f9fadc6519b789bc09e37583a34694
Nnamani Stephen <sir.stephennnamani@gmail.com>
2024-01-04 22:11:50 +0100

    Allow the change in Repository... all in one folder

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+double variable();
 int main() {
     printf("Hello, World!\n");
     printf("Forever");
+
     return 0;
+}
+double variable(){
+    char This_is_an_alphabet = 'a';
+    char string[] = "I am a string, officially an array of characters";
+    int Number = 1223;
+    float IamADecimal = 12.34;
+    printf("%d", "%s", This_is_an_alphabet, string);
+    double newNumber = Number + IamADecimal;
+    return newNumber;
 }
