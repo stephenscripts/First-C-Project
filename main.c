7deb6b33665cf2a08e1f05e2c31043675d8d57ec
Nnamani Stephen <sir.stephennnamani@gmail.com>
2024-01-05 23:09:43 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
-double variable();
+
 int main() {
+
     printf("Hello, World!\n");
     printf("Forever");
-
     return 0;
 }
-double variable(){
+void variable(){
     char This_is_an_alphabet = 'a';
     char string[] = "I am a string, officially an array of characters";
     int Number = 1223;
     float IamADecimal = 12.34;
     printf("%d", "%s", This_is_an_alphabet, string);
     double newNumber = Number + IamADecimal;
-    return newNumber;
 }
